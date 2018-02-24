746a71393521b57c03b9bd62c72c261b0b8eed89
iblesa <isma.work@gmail.com>
2018-02-24 11:02:38 +0000

    Adding Settings Activity Adding Menu to launch Settings Activity Setting parent activity for SettingsActivity

--- app/src/main/java/com/iblesa/movieapp/MainActivity.java
+++ app/src/main/java/com/iblesa/movieapp/MainActivity.java
@@ -2,6 +2,8 @@ package com.iblesa.movieapp;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +11,14 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        /* Use AppCompatActivity's method getMenuInflater to get a handle on the menu inflater */
+        MenuInflater inflater = getMenuInflater();
+        /* Use the inflater's inflate method to inflate our movies_menu layout to this menu */
+        inflater.inflate(R.menu.movies_menu, menu);
+        /* Return true so that the movies_menu is displayed in the Toolbar */
+        return true;
     }
 }
