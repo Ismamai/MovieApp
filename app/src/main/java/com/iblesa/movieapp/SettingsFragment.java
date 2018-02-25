52d83ff494ad4e72f9139909ac594aa599ce3c57
iblesa <isma.work@gmail.com>
2018-02-25 11:15:35 +0000

    Adding summary to preference Sort

--- app/src/main/java/com/iblesa/movieapp/SettingsFragment.java
+++ app/src/main/java/com/iblesa/movieapp/SettingsFragment.java
@@ -1,9 +1,14 @@
 package com.iblesa.movieapp;
 
 
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.support.v7.preference.CheckBoxPreference;
+import android.support.v7.preference.ListPreference;
+import android.support.v7.preference.Preference;
 import android.support.v7.preference.PreferenceFragmentCompat;
+import android.support.v7.preference.PreferenceScreen;
 
 
 /**
@@ -14,5 +19,28 @@ public class SettingsFragment extends PreferenceFragmentCompat {
     @Override
     public void onCreatePreferences(Bundle savedInstanceState, String rootKey) {
         addPreferencesFromResource(R.xml.preferences);
+
+        PreferenceScreen preferenceScreen = getPreferenceScreen();
+        SharedPreferences sharedPreferences = preferenceScreen.getSharedPreferences();
+        int count = preferenceScreen.getPreferenceCount();
+
+        for (int i = 0; i < count; i++) {
+            Preference preference = preferenceScreen.getPreference(i);
+            if (!(preference instanceof CheckBoxPreference)) {
+                String value = sharedPreferences.getString(preference.getKey(), "");
+                setPreferenceSummary(preference, value);
+
+            }
+        }
+    }
+
+    private void setPreferenceSummary(Preference preference, String value) {
+        if (preference instanceof ListPreference) {
+            ListPreference listPreference = (ListPreference) preference;
+            int indexOfValue = listPreference.findIndexOfValue(value);
+            if (indexOfValue >= 0) {
+                listPreference.setSummary(listPreference.getEntries()[indexOfValue]);
+            }
+        }
     }
 }
