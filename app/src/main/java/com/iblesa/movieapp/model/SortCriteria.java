86f315e71c5a708c60f0bf63f03779fd987718df
iblesa <isma.work@gmail.com>
2018-03-10 14:45:59 +0000

    Fixing SortCriteria. It was not working as expected

---
+++ app/src/main/java/com/iblesa/movieapp/model/SortCriteria.java
@@ -0,0 +1,31 @@
+package com.iblesa.movieapp.model;
+
+import java.util.Arrays;
+import java.util.List;
+
+/**
+ * SortCriteria to store the allowed criterias
+ */
+
+public class SortCriteria {
+    //Constants
+    public static final String POPULAR = "popular";
+    public static final String RATE = "rate";
+
+    private static final List<String> ALLOWED_CRITERIAS = Arrays.asList(POPULAR, RATE);
+
+    private final String criteria;
+
+
+
+    public SortCriteria(String criteria) {
+        if (!ALLOWED_CRITERIAS.contains(criteria)) {
+            throw new IllegalArgumentException("Criteria is not allowed. Requested " + criteria + " but allowed are " + ALLOWED_CRITERIAS);
+        }
+        this.criteria = criteria;
+    }
+
+    public String getCriteria() {
+        return criteria;
+    }
+}
