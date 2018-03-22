2c349e5e0ced118055844b1a63a46f1d6681a1ed
iblesa <isma.work@gmail.com>
2018-03-22 20:30:18 +0000

    showing data if everything went ok or error message if something failed.

--- app/src/main/java/com/iblesa/movieapp/MainActivity.java
+++ app/src/main/java/com/iblesa/movieapp/MainActivity.java
@@ -2,6 +2,9 @@ package com.iblesa.movieapp;
 
 import android.content.Intent;
 import android.content.SharedPreferences;
+import android.net.Uri;
+import android.os.AsyncTask;
+import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.preference.PreferenceManager;
@@ -17,9 +20,16 @@ import android.widget.TextView;
 
 import com.iblesa.movieapp.model.Movie;
 import com.iblesa.movieapp.model.SortCriteria;
-import com.iblesa.movieapp.network.MovieAPI;
-import com.iblesa.movieapp.util.FakeMoviesData;
+import com.iblesa.movieapp.util.MovieParser;
+
+import org.json.JSONException;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.HttpURLConnection;
+import java.net.URL;
 import java.util.List;
+import java.util.Scanner;
 
 public class MainActivity extends AppCompatActivity implements SharedPreferences.OnSharedPreferenceChangeListener{
 
@@ -35,12 +45,14 @@ public class MainActivity extends AppCompatActivity implements SharedPreferences
         setContentView(R.layout.activity_main);
         setupSharedPreferences();
         recyclerView = findViewById(R.id.rv_movies);
+        errorMessageDisplay = findViewById(R.id.tv_error_message_display);
+        progressBar = findViewById(R.id.pb_loading_indicator);
         GridLayoutManager gridLayoutManager = new GridLayoutManager(this, 2);
         recyclerView.setLayoutManager(gridLayoutManager);
         String key = getString(R.string.themoviedb_api_key);
         MoviesAdapter adapter = new MoviesAdapter();
         recyclerView.setAdapter(adapter);
-        MovieAPI api = new MovieAPI(key, adapter, this);
+        MovieAPI api = new MovieAPI(key, adapter);
         SharedPreferences defaultSharedPreferences = PreferenceManager.getDefaultSharedPreferences(this);
         String criteria = defaultSharedPreferences.getString(getString(R.string.preference_sort_key), getString(R.string.preference_sort_value_popular));
         SortCriteria sortCriteria;
@@ -99,14 +111,110 @@ public class MainActivity extends AppCompatActivity implements SharedPreferences
     }
 
 
-    public void showError() {
+    private void showError() {
         recyclerView.setVisibility(View.INVISIBLE);
         errorMessageDisplay.setVisibility(View.VISIBLE);
     }
 
 
-    public void showData() {
+    private void showData() {
         recyclerView.setVisibility(View.VISIBLE);
         errorMessageDisplay.setVisibility(View.INVISIBLE);
+    }
+
+    /**
+     * This class will deal with network interaction with TheMovieDB api
+     */
+
+    class MovieAPI extends AsyncTask<SortCriteria, Void, List<Movie>> {
+
+        private static final String BASE_URL = "http://api.themoviedb.org/3/movie/";
+        private static final String POPULAR_URL = "popular";
+        private static final String RATE_URL = "top_rated";
+        private static final String API_KEY_PARAM = "api_key";
+
+        private String param;
+        private MoviesAdapter moviesAdapter;
+
+
+        MovieAPI(String param, MoviesAdapter moviesAdapter) {
+            this.param = param;
+            this.moviesAdapter = moviesAdapter;
+        }
+
+
+        @Override
+        protected List<Movie> doInBackground(SortCriteria... sortTypes) {
+            if (sortTypes.length < 1) {
+                throw new IllegalArgumentException("Missing SortCriteria parameter");
+            }
+            String sortCriteria = sortTypes[0].getCriteria();
+            switch (sortCriteria) {
+                case SortCriteria.POPULAR: {
+                    Uri uri = Uri.parse(BASE_URL).buildUpon().appendPath(POPULAR_URL).appendQueryParameter(API_KEY_PARAM, param).build();
+                    return getMoviesFromUrl(uri);
+
+                }
+                case SortCriteria.RATE: {
+                    Uri uri = Uri.parse(BASE_URL).buildUpon().appendPath(RATE_URL).appendQueryParameter(API_KEY_PARAM, param).build();
+                    return getMoviesFromUrl(uri);
+                }
+                default:
+                    throw new IllegalArgumentException("SortCriteria not supported " + sortCriteria);
+
+            }
+        }
+
+        @Nullable
+        private List<Movie> getMoviesFromUrl(Uri uri) {
+            try {
+                String responseFromHttpUrl = getResponseFromHttpUrl(new URL(uri.toString()));
+                return MovieParser.parseJSON(responseFromHttpUrl);
+            } catch (IOException e) {
+                Log.e(Constants.TAG, "Error getting external content for url " + uri.toString(), e);
+                return null;
+            } catch (JSONException e) {
+                Log.e(Constants.TAG, "Error parsing json", e);
+                return null;
+            }
+        }
+
+
+        /**
+         * This method returns the entire result from the HTTP response.
+         *
+         * @param url The URL to fetch the HTTP response from.
+         * @return The contents of the HTTP response.
+         * @throws IOException Related to network and stream reading
+         */
+        private String getResponseFromHttpUrl(URL url) throws IOException {
+            HttpURLConnection urlConnection = (HttpURLConnection) url.openConnection();
+            try {
+                InputStream in = urlConnection.getInputStream();
+
+                Scanner scanner = new Scanner(in);
+                scanner.useDelimiter("\\A");
+
+                boolean hasInput = scanner.hasNext();
+                if (hasInput) {
+                    return scanner.next();
+                } else {
+                    return null;
+                }
+            } finally {
+                urlConnection.disconnect();
+            }
+        }
+
+        @Override
+        protected void onPostExecute(List<Movie> movies) {
+            if (movies != null) {
+                showData();
+                moviesAdapter.setMovies(movies);
+            } else {
+                showError();
+            }
+        }
+
     }
 }
