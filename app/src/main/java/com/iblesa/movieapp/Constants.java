642add4efd1a5957c025db3cedf3dd4c9bc0aaa1
iblesa <isma.work@gmail.com>
2018-03-24 16:04:19 +0000

    Adding some constants

--- app/src/main/java/com/iblesa/movieapp/Constants.java
+++ app/src/main/java/com/iblesa/movieapp/Constants.java
@@ -6,4 +6,6 @@ package com.iblesa.movieapp;
 
 public class Constants {
     public static final String TAG = "iblesa_app";
+    public static final String MOVIES = "movies_list";
+    public static final String MOVIE_DETAIL = "movie_detail";
 }
