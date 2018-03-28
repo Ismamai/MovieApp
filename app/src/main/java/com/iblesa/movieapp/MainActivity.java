015f3efa07b5370856cb711316056d9a28618e90
iblesa <isma.work@gmail.com>
2018-03-28 20:07:39 +0100

    SUGGESTION It is recommended to have this AsyncTask class into a separate file to make your code more maintainable. Here is a nice tutorial that will show you how to do this: http://www.jameselsey.co.uk/blogs/techblog/extracting-out-your-asynctasks-into-separate-classes-makes-your-code-cleaner/

--- app/src/main/java/com/iblesa/movieapp/MainActivity.java
+++ app/src/main/java/com/iblesa/movieapp/MainActivity.java
@@ -5,9 +5,6 @@ import android.content.Intent;
 import android.content.SharedPreferences;
 import android.net.ConnectivityManager;
 import android.net.NetworkInfo;
-import android.net.Uri;
-import android.os.AsyncTask;
-import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.preference.PreferenceManager;
@@ -23,22 +20,18 @@ import android.widget.TextView;
 
 import com.iblesa.movieapp.model.Movie;
 import com.iblesa.movieapp.model.SortCriteria;
-import com.iblesa.movieapp.util.MovieParser;
+import com.iblesa.movieapp.network.AsyncTaskCompleteListener;
+import com.iblesa.movieapp.network.MovieAPI;
 
-import org.json.JSONException;
-
-import java.io.IOException;
-import java.io.InputStream;
-import java.net.HttpURLConnection;
-import java.net.URL;
 import java.util.List;
-import java.util.Scanner;
+
 
 public class MainActivity extends AppCompatActivity implements SharedPreferences.OnSharedPreferenceChangeListener, MoviesAdapter.ListItemClickListener{
 
     private RecyclerView recyclerView;
     private TextView errorMessageDisplay;
     private ProgressBar progressBar;
+    private MoviesAdapter moviesAdapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -51,13 +44,13 @@ public class MainActivity extends AppCompatActivity implements SharedPreferences
         GridLayoutManager gridLayoutManager = new GridLayoutManager(this, 2);
         recyclerView.setLayoutManager(gridLayoutManager);
         String key = getString(R.string.themoviedb_api_key);
-        MoviesAdapter adapter = new MoviesAdapter(this);
-        recyclerView.setAdapter(adapter);
-        loadData(key, adapter);
+        moviesAdapter = new MoviesAdapter(this);
+        recyclerView.setAdapter(moviesAdapter);
+        loadData(key, moviesAdapter);
     }
 
     private void loadData(String key, MoviesAdapter adapter) {
-        MovieAPI api = new MovieAPI(key, adapter);
+
         SharedPreferences defaultSharedPreferences = PreferenceManager.getDefaultSharedPreferences(this);
         String criteria = defaultSharedPreferences.getString(getString(R.string.preference_sort_key), getString(R.string.preference_sort_value_popular));
         SortCriteria sortCriteria;
@@ -76,6 +69,7 @@ public class MainActivity extends AppCompatActivity implements SharedPreferences
         boolean isConnected = activeNetwork != null &&
                 activeNetwork.isConnectedOrConnecting();
         if (isConnected) {
+            MovieAPI api = new MovieAPI(key, new FetchMyDataTaskCompleteListener());
             api.execute(sortCriteria);
         } else {
             showError();
@@ -146,98 +140,10 @@ public class MainActivity extends AppCompatActivity implements SharedPreferences
         startActivity(intentDetailActivity);
     }
 
-    /**
-     * This class will deal with network interaction with TheMovieDB api
-     */
-
-    class MovieAPI extends AsyncTask<SortCriteria, Void, List<Movie>> {
-
-        private static final String BASE_URL = "http://api.themoviedb.org/3/movie/";
-        private static final String POPULAR_URL = "popular";
-        private static final String RATE_URL = "top_rated";
-        private static final String API_KEY_PARAM = "api_key";
-
-        private String param;
-        private MoviesAdapter moviesAdapter;
-
-
-        MovieAPI(String param, MoviesAdapter moviesAdapter) {
-            this.param = param;
-            this.moviesAdapter = moviesAdapter;
-        }
-
+    public class FetchMyDataTaskCompleteListener implements AsyncTaskCompleteListener<List<Movie>> {
 
         @Override
-        protected List<Movie> doInBackground(SortCriteria... sortTypes) {
-            if (sortTypes.length < 1) {
-                throw new IllegalArgumentException("Missing SortCriteria parameter");
-            }
-            String sortCriteria = sortTypes[0].getCriteria();
-            switch (sortCriteria) {
-                case SortCriteria.POPULAR: {
-                    Uri uri = Uri.parse(BASE_URL).buildUpon().appendPath(POPULAR_URL).appendQueryParameter(API_KEY_PARAM, param).build();
-                    return getMoviesFromUrl(uri);
-
-                }
-                case SortCriteria.RATE: {
-                    Uri uri = Uri.parse(BASE_URL).buildUpon().appendPath(RATE_URL).appendQueryParameter(API_KEY_PARAM, param).build();
-                    return getMoviesFromUrl(uri);
-                }
-                default:
-                    throw new IllegalArgumentException("SortCriteria not supported " + sortCriteria);
-
-            }
-        }
-
-        @Nullable
-        private List<Movie> getMoviesFromUrl(Uri uri) {
-            try {
-                String responseFromHttpUrl = getResponseFromHttpUrl(new URL(uri.toString()));
-                return MovieParser.parseJSON(responseFromHttpUrl);
-            } catch (IOException e) {
-                Log.e(Constants.TAG, "Error getting external content for url " + uri.toString(), e);
-                return null;
-            } catch (JSONException e) {
-                Log.e(Constants.TAG, "Error parsing json", e);
-                return null;
-            }
-        }
-
-
-        /**
-         * This method returns the entire result from the HTTP response.
-         *
-         * @param url The URL to fetch the HTTP response from.
-         * @return The contents of the HTTP response.
-         * @throws IOException Related to network and stream reading
-         */
-        private String getResponseFromHttpUrl(URL url) throws IOException {
-            HttpURLConnection urlConnection = (HttpURLConnection) url.openConnection();
-            try {
-                InputStream in = urlConnection.getInputStream();
-
-                Scanner scanner = new Scanner(in);
-                scanner.useDelimiter("\\A");
-
-                boolean hasInput = scanner.hasNext();
-                if (hasInput) {
-                    return scanner.next();
-                } else {
-                    return null;
-                }
-            } finally {
-                urlConnection.disconnect();
-            }
-        }
-
-        @Override
-        protected void onPreExecute() {
-            super.onPreExecute();
-            progressBar.setVisibility(View.VISIBLE);
-        }
-
-        @Override
-        protected void onPostExecute(List<Movie> movies) {
+        public void onTaskComplete(List<Movie> movies) {
             progressBar.setVisibility(View.INVISIBLE);
             if (movies != null) {
                 showData();
@@ -247,5 +153,9 @@ public class MainActivity extends AppCompatActivity implements SharedPreferences
             }
         }
 
+        @Override
+        public void onTaskPreExecute() {
+            progressBar.setVisibility(View.VISIBLE);
+        }
     }
 }
---
+++ app/src/main/java/com/iblesa/movieapp/network/MovieAPI.java
@@ -0,0 +1,114 @@
+package com.iblesa.movieapp.network;
+
+import android.net.Uri;
+import android.os.AsyncTask;
+import android.support.annotation.Nullable;
+import android.util.Log;
+
+import com.iblesa.movieapp.Constants;
+import com.iblesa.movieapp.model.Movie;
+import com.iblesa.movieapp.model.SortCriteria;
+import com.iblesa.movieapp.util.MovieParser;
+
+import org.json.JSONException;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.util.List;
+import java.util.Scanner;
+
+/**
+ * This class will deal with network interaction with TheMovieDB api
+ */
+public class MovieAPI extends AsyncTask<SortCriteria, Void, List<Movie>> {
+    private static final String BASE_URL = "http://api.themoviedb.org/3/movie/";
+    private static final String POPULAR_URL = "popular";
+    private static final String RATE_URL = "top_rated";
+    private static final String API_KEY_PARAM = "api_key";
+
+    private String apiKey;
+    private AsyncTaskCompleteListener<List<Movie>> listener;
+
+
+    public MovieAPI(String apiKey, AsyncTaskCompleteListener<List<Movie>> listener) {
+        this.apiKey = apiKey;
+        this.listener = listener;
+    }
+
+
+    @Override
+    protected List<Movie> doInBackground(SortCriteria... sortTypes) {
+        if (sortTypes.length < 1) {
+            throw new IllegalArgumentException("Missing SortCriteria parameter");
+        }
+        String sortCriteria = sortTypes[0].getCriteria();
+        switch (sortCriteria) {
+            case SortCriteria.POPULAR: {
+                Uri uri = Uri.parse(BASE_URL).buildUpon().appendPath(POPULAR_URL).appendQueryParameter(API_KEY_PARAM, apiKey).build();
+                return getMoviesFromUrl(uri);
+
+            }
+            case SortCriteria.RATE: {
+                Uri uri = Uri.parse(BASE_URL).buildUpon().appendPath(RATE_URL).appendQueryParameter(API_KEY_PARAM, apiKey).build();
+                return getMoviesFromUrl(uri);
+            }
+            default:
+                throw new IllegalArgumentException("SortCriteria not supported " + sortCriteria);
+
+        }
+    }
+
+    @Nullable
+    private List<Movie> getMoviesFromUrl(Uri uri) {
+        try {
+            String responseFromHttpUrl = getResponseFromHttpUrl(new URL(uri.toString()));
+            return MovieParser.parseJSON(responseFromHttpUrl);
+        } catch (IOException e) {
+            Log.e(Constants.TAG, "Error getting external content for url " + uri.toString(), e);
+            return null;
+        } catch (JSONException e) {
+            Log.e(Constants.TAG, "Error parsing json", e);
+            return null;
+        }
+    }
+
+
+    /**
+     * This method returns the entire result from the HTTP response.
+     *
+     * @param url The URL to fetch the HTTP response from.
+     * @return The contents of the HTTP response.
+     * @throws IOException Related to network and stream reading
+     */
+    private String getResponseFromHttpUrl(URL url) throws IOException {
+        HttpURLConnection urlConnection = (HttpURLConnection) url.openConnection();
+        try {
+            InputStream in = urlConnection.getInputStream();
+
+            Scanner scanner = new Scanner(in);
+            scanner.useDelimiter("\\A");
+
+            boolean hasInput = scanner.hasNext();
+            if (hasInput) {
+                return scanner.next();
+            } else {
+                return null;
+            }
+        } finally {
+            urlConnection.disconnect();
+        }
+    }
+
+    @Override
+    protected void onPreExecute() {
+        super.onPreExecute();
+        listener.onTaskPreExecute();
+    }
+
+    @Override
+    protected void onPostExecute(List<Movie> movies) {
+        listener.onTaskComplete(movies);
+    }
+}
