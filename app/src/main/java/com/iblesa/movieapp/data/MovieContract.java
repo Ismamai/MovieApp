f1604bba75807187f5c3e9b06036ae12461e60de
iblesa <isma.work@gmail.com>
2018-03-30 13:36:14 +0100

    Adding back classes for ContentProvider

---
+++ app/src/main/java/com/iblesa/movieapp/data/MovieContract.java
@@ -0,0 +1,28 @@
+package com.iblesa.movieapp.data;
+
+import android.provider.BaseColumns;
+
+/**
+ * Database contract for Movie
+ */
+public class MovieContract {
+    /**
+     * Constructor should never be invoked
+     */
+    private MovieContract() {
+    }
+
+    public static class MovieEntry implements BaseColumns {
+        public static final String TABLE_NAME = "movies";
+
+        public static final String COLUMN_TITLE = "title";
+        public static final String COLUMN_RELEASE_DATE = "release_date";
+        public static final String COLUMN_OVERVIEW = "overview";
+        public static final String COLUMN_POPULARITY = "popularity";
+        public static final String COLUMN_VOTE_AVERAGE = "vote_average";
+        public static final String COLUMN_POSTER_PATH = "poster_path";
+        public static final String COLUMN_BACKDROP_PATH = "backdrop_path";
+        public static final String COLUMN_VIDEO = "video";
+
+    }
+}
---
+++ app/src/main/java/com/iblesa/movieapp/data/MovieDBHelper.java
@@ -0,0 +1,55 @@
+package com.iblesa.movieapp.data;
+
+import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+import android.util.Log;
+
+import com.iblesa.movieapp.Constants;
+import com.iblesa.movieapp.MainActivity;
+import com.iblesa.movieapp.data.MovieContract.MovieEntry;
+
+/**
+ * Movie class to deal with db
+ */
+
+public class MovieDBHelper extends SQLiteOpenHelper {
+
+    // File that will contain the database
+    private static final String DATABASE_NAME = "movies.db";
+    // database version
+    private static final int DATABASE_VERSION = 1;
+
+
+    public MovieDBHelper(Context context) {
+        // Call the parent constructor
+        super(context, DATABASE_NAME, null, DATABASE_VERSION);
+    }
+
+    @Override
+    public void onCreate(SQLiteDatabase db) {
+
+        final String SQL_CREATE_MOVIE_TABLE = "CREATE TABLE " +
+                MovieEntry.TABLE_NAME + " (" +
+                MovieEntry._ID + " INTEGER UNIQUE NOT NULL, " +
+                MovieEntry.COLUMN_TITLE + " TEXT NOT NULL, " +
+                MovieEntry.COLUMN_RELEASE_DATE + " TEXT NOT NULL, " +
+                MovieEntry.COLUMN_OVERVIEW + " TEXT NOT NULL, " +
+                MovieEntry.COLUMN_POPULARITY + " REAL NOT NULL, " +
+                MovieEntry.COLUMN_VOTE_AVERAGE + " REAL NOT NULL, " +
+                MovieEntry.COLUMN_VIDEO + " INTEGER, " +
+                MovieEntry.COLUMN_POSTER_PATH + " TEXT NOT NULL, " +
+                MovieEntry.COLUMN_BACKDROP_PATH + " TEXT NOT NULL " +
+                ");";
+        Log.d(Constants.TAG, SQL_CREATE_MOVIE_TABLE);
+        db.execSQL(SQL_CREATE_MOVIE_TABLE);
+    }
+
+    @Override
+    public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+        String DROP_STATEMENT = "DROP TABLE IF EXISTS " + MovieEntry.TABLE_NAME;
+        db.execSQL(DROP_STATEMENT);
+        Log.d(Constants.TAG, DROP_STATEMENT);
+        onCreate(db);
+    }
+}
